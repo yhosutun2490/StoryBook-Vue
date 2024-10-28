82bac3fa90a1dd337ad29fa2ebe0b4524d13d7a1
yhosutun2490 <yhosutun2491@gmail.com>
2024-10-28 18:23:56 +0800

    <feat> 設定primevue客製樣式

--- src/components/Paginator/Paginator.vue
+++ src/components/Paginator/Paginator.vue
@@ -7,51 +7,86 @@
       width: fit-content; 
       `
     },
-    content: {
-      class: 'content',
-      style: `border: none; 
-      width: fit-content; 
-      height: 40px;
-      `
-
-    },
-    first: {
-      style: buttonStyle + borderLeft,
-    },
-    prev: {
-      style: buttonStyle
-    },
-    next: {
-      style: buttonStyle
-    },
-    last: {
-      style: buttonStyle + borderRight,
-    }
-    
+    content: 'paginator-wrap',
+    first: 'd-none',
+    prev: 'paginator-button prev',
+    next: 'paginator-button next',
+    last: 'd-none',
+    pages: 'custom-pages-link',
+    page: 'custom-page-link'
+   
   }"
+  :totalRecords="totalRecords"
+  :rows="rows"
+  :pageLinkSize="pageLinkSize"
   ></Paginator>
 </template>
 <script setup>
 import Paginator from 'primevue/paginator';
-const buttonStyle = `
-      color: black; 
-      width: 40px; 
-      height: 100%;
-      font-weight: 500;
-      border: none;
-      background: rgba(234, 243, 244, 1);
-      cursor: pointer;
-      `
-const borderLeft = `
-border-top-left-radius: 8px;
-border-bottom-left-radius: 8px;
-`
+import { defineProps } from "vue";
 
-const borderRight = `
-border-top-right-radius: 8px;
-border-bottom-right-radius: 8px;
-`
+defineProps({
+  totalRecords: {
+    type: Number,
+    default: 15
+  },
+  rows:  {
+    type: Number,
+    default: 5
+  },
+  pageLinkSize:  {
+    type: Number,
+    default: 3
+  }
+
+})
 </script>
 
+<style lang="scss">
+.paginator-warp {
+  border: none; 
+  width: fit-content; 
+  height: 40px;
+}
+.custom-pages-link{
 
+  & > :first-child {
+    border: none;
+    border-left: 1px solid grey;
+    border-right: 1px solid grey;
+  }
+  & > * {
+    border: none;
+    border-right: 1px solid grey;
+  }
+}
+.custom-page-link {
+  text-align: center;
+  position: relative;
+  top: -1px;
+  width: 40px; 
+  height: 40px;
+  background: rgba(234, 243, 244, 1);
+}
+.d-none {
+  display: none;
+}
+.paginator-button {
+  color: black; 
+  width: 40px; 
+  height: 40px;
+  font-weight: 500;
+  border: none;
+  background: rgba(234, 243, 244, 1);
+  cursor: pointer;
+}
+.prev {
+  border-top-left-radius: 8px;
+  border-bottom-left-radius: 8px;
+}
+.next {
+  border-top-right-radius: 8px;
+  border-bottom-right-radius: 8px;
+}
+</style>
 
