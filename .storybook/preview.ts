db22fe195edbf3c177207524cadcfceafe69505f
yhosutun2490 <yhosutun2491@gmail.com>
2024-10-28 09:16:01 +0800

    <feat> 安裝primevue和解析器

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,16 +1,19 @@
 /** @type { import('@storybook/vue3').Preview } */
 import { type Preview, setup } from '@storybook/vue3'
-import {setup} from '@storybook/vue3'
-
 import PrimeVue from 'primevue/config'
-
+import { fn } from '@storybook/test';
 setup((app) => {
   app.use(PrimeVue)
 })
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: '^on[A-Z].*' },
+    actions: { 
+      // argTypesRegex: '^on[A-Z].*' 
+      // 移除 argTypesRegex，並顯式定義 action
+      onClick: fn(),
+      onMouseOver: fn(),
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
