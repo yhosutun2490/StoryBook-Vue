98959c283afe70c6b3a908f11786c20973b740f8
yhosutun2491 <yhosutun2491@gmail.com>
2024-10-31 19:00:31 +0800

    <feat> 註冊paginaotr至Vue

--- src/primevue.js
+++ src/primevue.js
@@ -1,2 +1,8 @@
 import Paginator from "./components/Paginator/Paginator.vue"
+// 添加 install 方法
+export default {
+  install(Vue) {
+    Vue.component('CustomPaginator', Paginator); // 註冊組件
+  }
+};
 export {Paginator}
