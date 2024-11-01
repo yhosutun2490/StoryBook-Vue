755a4dc07b33a2dcb43a8b497eff1fc4ff692921
yhosutun2490 <yhosutun2491@gmail.com>
2024-11-01 15:54:57 +0800

    <fix> 移除install 增加版號0.0.7

--- src/primevue.js
+++ src/primevue.js
@@ -1,8 +1,3 @@
 import Paginator from "./components/Paginator/Paginator.vue"
 // 添加 install 方法
-export default {
-  install(Vue) {
-    Vue.component('CustomPaginator', Paginator); // 註冊組件
-  }
-};
 export {Paginator}
