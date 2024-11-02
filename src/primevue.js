561771ed43ea424f423f55997bd6b814a7f0ad0d
yhosutun2491 <yhosutun2491@gmail.com>
2024-11-02 11:33:32 +0800

    <feat> 註冊一般mybutton

--- src/primevue.js
+++ src/primevue.js
@@ -1,11 +1,14 @@
 import Paginator from "./components/Paginator/Paginator.vue"
+import MyButton from "./components/MyButton/MyButton.vue";
 import PrimeVue from "primevue/config";
 // 添加 install 方法
 export default  {
   install: (app)=> {
     app.use(PrimeVue,{ unstyled: true })
     app.component("CustomPaginator", Paginator);
+    app.component("MyButton", MyButton);
+
   }
 }
 
-export {Paginator}
+export {Paginator,MyButton}
